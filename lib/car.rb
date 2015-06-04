2b31e0bd100ed65ff5e5300a5d280a27677f8e63
Tae Kim <taekimsf@gmail.com>
2015-06-03 18:22:46 -0700

    hella easy

--- lib/car.rb
+++ lib/car.rb
@@ -1,3 +1,13 @@
 class Car
   #write Car class code here
+  # make speed attribute
+  attr_accessor :speed
+  # car speed is 0 when initialized
+  def initialize
+  	@speed = 0
+  end
+  # car speed is equal to given speed
+  def accelerate(speed = 0)
+  	@speed = speed
+  end
 end
--- lib/race.rb
+++ lib/race.rb
@@ -2,4 +2,22 @@ require_relative 'car'
 
 class Race
   #write Race class code here
+  # initialize creates two cars
+  attr_accessor :cars
+  def initialize
+  	car1 = Car.new
+  	car2 = Car.new
+  	car1.speed = rand(0..100)
+  	car2.speed = rand(0..100)
+  	@cars = []
+  	@cars.push(car1).push(car2)
+  end
+  def winner
+    return @cars[0] if @cars[0].speed > @cars[1].speed
+    return @cars[1] if @cars[1].speed > @cars[0].speed
+  end
+  def loser
+  	return @cars[1] if @cars[0].speed > @cars[1].speed
+    return @cars[0] if @cars[1].speed > @cars[0].speed
+  end
 end
